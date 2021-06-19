3fd4af32f77de36be546086808704764fb20b43f
felix.andrean01 <felix.andrean17@gmail.com>
2021-06-19 12:19:24 +0700

    add conditional rendering if new movie not available and add atrributes cypress

--- src/Pages/Home/HomeHooks.tsx
+++ src/Pages/Home/HomeHooks.tsx
@@ -9,7 +9,7 @@ export const HomeHooks: FC<any> = (props: any) => {
   const { moviesResponse, toMovieDetail } = useHomeHooks(props);
   return (
     <div>
-      <div className={styles["full_landing_page"]}>
+      <div className={styles["full_landing_page"]} data-cy="landing-page">
         <div className={styles["landing_page_title"]}>
           <Text variant="h1">
             Find <b>Perfect</b> Movies For You
@@ -20,25 +20,33 @@ export const HomeHooks: FC<any> = (props: any) => {
         </div>
       </div>
       <div className="desktop-boxed">
-        <div className={styles["new_relese_movie"]}>
-          <Text variant="h3">New Release</Text>
-          <div className={styles["home_movies_list"]}>
-            {moviesResponse.map((data: any, index: number) => (
-              <div
-                className={styles["movies_list_item"]}
-                key={index}
-                onClick={() => toMovieDetail(data)}
-              >
-                <MoviePoster
-                  poster={data.Poster}
-                  title={data.Title}
-                  type={data.Type}
-                  year={data.Year}
-                />
-              </div>
-            ))}
+        {!!moviesResponse.length && (
+          <div
+            className={styles["new_relese_movie"]}
+            data-cy="new_release_frame"
+          >
+            <Text variant="h3">New Release</Text>
+            <div
+              className={styles["home_movies_list"]}
+              data-cy="new_release_container"
+            >
+              {moviesResponse.map((data: any, index: number) => (
+                <div
+                  className={styles["movies_list_item"]}
+                  key={index}
+                  onClick={() => toMovieDetail(data)}
+                >
+                  <MoviePoster
+                    poster={data.Poster}
+                    title={data.Title}
+                    type={data.Type}
+                    year={data.Year}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
